030699aecd734438076f264e3acd3cf6af331330
newiexk-cyber <newiexk@gmail.com>
2026-08-07 17:16:00 +0700

    Add files via upload

---
+++ custom-tiemanh.js
@@ -0,0 +1,10 @@
+// In ra console để test
+console.log("Hệ thống Script tùy chỉnh đã được tải thành công!");
+
+// Ví dụ 1 đoạn code: Tự động thêm viền đỏ cho tất cả ảnh trên trang
+// setTimeout(() => {
+//    const images = document.querySelectorAll('img');
+//    images.forEach(img => {
+//        img.style.border = "5px solid red";
+//    });
+// }, 2000);
